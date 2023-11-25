ae33569a877c75a5de749f4b00573c9aad519f9c
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-11-25 16:23:04 -0600

    Define interface ProdDrawing
    Drawings are usually high-level, so the details of GraphicContext should be hidden to focus in mathematical geometrical patterns and definitions to draw, instead of weird and cumbersome Canvas APIs.

---
+++ javafx/src/main/java/engineer/mathsoftware/canvasplay/ProdDrawing.java
@@ -0,0 +1,10 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/canvas-playground
+
+package engineer.mathsoftware.canvasplay;
+
+/***
+ * Defines production high-level drawing APIs for the {@link ProdCanvas}.
+ */
+public interface ProdDrawing {}
