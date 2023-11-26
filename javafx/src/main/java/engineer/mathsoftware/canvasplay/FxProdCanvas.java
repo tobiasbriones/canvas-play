8b5424f60350d1cd9c9319bcc279ac27cbc73348
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-11-25 21:48:27 -0600

    Implement interface ProdCanvas into FxProdCanvas

---
+++ javafx/src/main/java/engineer/mathsoftware/canvasplay/FxProdCanvas.java
@@ -0,0 +1,40 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/canvas-playground
+
+package engineer.mathsoftware.canvasplay;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
+/***
+ * Provides a production level {@link javafx.scene.canvas.Canvas} useful for
+ * building relatively professional productions like images and animations
+ * with scaling for custom resolutions, etc.
+ */
+public class FxProdCanvas implements ProdCanvas {
+    private final Canvas canvas;
+    private final double scale;
+
+    public FxProdCanvas(Canvas canvas, double scale) {
+        this.canvas = canvas;
+        this.scale = scale;
+    }
+
+    @Override
+    public double width() { return canvas.getWidth() / scale; }
+
+    @Override
+    public double height() { return canvas.getHeight() / scale; }
+
+    @Override
+    public double cx() { return width() / 2.0; }
+
+    @Override
+    public double cy() { return height() / 2.0; }
+
+    @Override
+    public GraphicsContext ctx() {
+        return canvas.getGraphicsContext2D();
+    }
+}
