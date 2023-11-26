1a5c1f48c90e298577a777ec54470e5c21ab6c8a
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-11-25 18:07:50 -0600

    Add utility class CommonDrawings with must-have drawing traits

---
+++ javafx/src/main/java/engineer/mathsoftware/canvasplay/drawing/CommonDrawings.java
@@ -0,0 +1,23 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/canvas-playground
+
+package engineer.mathsoftware.canvasplay.drawing;
+
+import javafx.scene.paint.Paint;
+
+public final class CommonDrawings {
+    @FunctionalInterface
+    public interface Fill {
+        void fill(Paint color);
+    }
+
+    @FunctionalInterface
+    public interface Stroke {
+        void stroke(Paint color);
+    }
+
+    public interface CommonDrawing extends Fill, Stroke {}
+
+    private CommonDrawings() {}
+}
