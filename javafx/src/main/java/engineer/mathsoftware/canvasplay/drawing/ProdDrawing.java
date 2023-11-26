75494854f68bcf68f2a5f6efa949eb08671e617c
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-11-25 21:51:37 -0600

    Add factory method to interface ProdDrawing

--- javafx/src/main/java/engineer/mathsoftware/canvasplay/drawing/ProdDrawing.java
+++ javafx/src/main/java/engineer/mathsoftware/canvasplay/drawing/ProdDrawing.java
@@ -5,7 +5,12 @@
 package engineer.mathsoftware.canvasplay.drawing;
 
 import engineer.mathsoftware.canvasplay.shape.Oval;
+import javafx.scene.canvas.GraphicsContext;
 
 public interface ProdDrawing {
+    static ProdDrawing of(GraphicsContext ctx) {
+        return new FxDrawings.Drawing(ctx);
+    }
+
     OvalDrawing oval(Oval oval);
 }
