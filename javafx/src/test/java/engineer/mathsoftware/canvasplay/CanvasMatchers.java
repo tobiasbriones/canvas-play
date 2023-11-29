1e081b76dae45e448dac48e6fd4af39392f6b8c0
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-11-29 16:48:21 -0600

    Implement Matcher method hasDrawing
    It checks that a Canvas is the exact same drawing being compared to.

--- javafx/src/test/java/engineer/mathsoftware/canvasplay/CanvasMatchers.java
+++ javafx/src/test/java/engineer/mathsoftware/canvasplay/CanvasMatchers.java
@@ -4,7 +4,88 @@
 
 package engineer.mathsoftware.canvasplay;
 
+import javafx.application.Platform;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import org.hamcrest.Matcher;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.IntStream;
+
+import static org.testfx.matcher.base.GeneralMatchers.typeSafeMatcher;
+
 public final class CanvasMatchers {
+    public static Matcher<Canvas> hasDrawing(Canvas drawing) {
+        return hasDrawing(drawing, "a specified drawing");
+    }
+
+    /**
+     * Creates a matcher that matches when the {@link Canvas} drawing is equal
+     * to the given Canvas drawing.
+     * <p>
+     * That is, it matches when both canvases have the exact graphics, compared
+     * pixel by pixel.
+     *
+     * @param drawing expected canvas to match
+     * @param name    short description of what's in the expected drawing
+     *
+     * @return a matcher that matches when two canvas graphics are exactly the
+     * same pixel by pixel
+     */
+    public static Matcher<Canvas> hasDrawing(Canvas drawing, String name) {
+        return typeSafeMatcher(
+            Canvas.class,
+            "has " + name,
+            canvas -> "Actual drawing doesn't match expected drawing",
+            canvas -> areDrawingEquals(canvas, drawing)
+        );
+    }
+
+    private static boolean areDrawingEquals(
+        Canvas actualCanvas,
+        Canvas expectedCanvas
+    ) {
+        // Assume canvas sizes are integers and both equals //
+        var expected = snapshot(expectedCanvas);
+        var actual = snapshot(actualCanvas);
+        var expectedPixels = expected.getPixelReader();
+        var actualPixels = actual.getPixelReader();
+        var width = (int) actualCanvas.getWidth();
+        var height = (int) actualCanvas.getHeight();
+
+        record ColorPair(Color a, Color b) {
+            boolean match() { return a.equals(b); }
+        }
+
+        return IntStream
+            .range(0, height)
+            .allMatch(y -> IntStream
+                .range(0, width)
+                .mapToObj(x -> new ColorPair(
+                    actualPixels.getColor(x, y),
+                    expectedPixels.getColor(x, y)
+                ))
+                .allMatch(ColorPair::match));
+    }
+
+    private static WritableImage snapshot(Canvas drawing) {
+        var result = new CompletableFuture<WritableImage>();
+
+        Platform.runLater(() -> {
+            var snapshot = drawing.snapshot(null, null);
+
+            result.complete(snapshot);
+        });
+
+        try {
+            return result.get();
+        }
+        catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private CanvasMatchers() {}
 }
