7d00dd91ceff7ad662b8f2b69ed1d5d6e48d421d
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-11-29 18:00:02 -0600

    Add app module-info.java

---
+++ javafx/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/blog
+
+module engineer.mathsoftware.canvasplay {
+    requires javafx.controls;
+    requires javafx.swing;
+    exports engineer.mathsoftware.canvasplay;
+}
