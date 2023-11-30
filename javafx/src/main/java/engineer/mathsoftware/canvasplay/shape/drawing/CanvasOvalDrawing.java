721e7c6ba8d0dd5c64c152b22781b7a81ce97ce3
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-11-29 20:50:53 -0600

    Implement interface OvalDrawing

---
+++ javafx/src/main/java/engineer/mathsoftware/canvasplay/shape/drawing/CanvasOvalDrawing.java
@@ -0,0 +1,54 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/canvas-playground
+
+package engineer.mathsoftware.canvasplay.shape.drawing;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Paint;
+
+final class CanvasOvalDrawing {
+    record CircleDrawing(
+        GraphicsContext ctx,
+        double radius,
+        double cx,
+        double cy,
+        double diameter
+    ) implements OvalDrawing {
+        @Override
+        public void stroke(Paint color) {
+            ctx.setStroke(color);
+            ctx.strokeOval(cx - radius, cy - radius, diameter, diameter);
+        }
+
+        @Override
+        public void fill(Paint color) {
+            ctx.setFill(color);
+            ctx.fillOval(cx - radius, cy - radius, diameter, diameter);
+        }
+    }
+
+    record EllipseDrawing(
+        GraphicsContext ctx,
+        double radiusX,
+        double radiusY,
+        double cx,
+        double cy,
+        double diameterX,
+        double diameterY
+    ) implements OvalDrawing {
+        @Override
+        public void stroke(Paint color) {
+            ctx.setStroke(color);
+            ctx.strokeOval(cx - radiusX, cy - radiusY, diameterX, diameterY);
+        }
+
+        @Override
+        public void fill(Paint color) {
+            ctx.setFill(color);
+            ctx.fillOval(cx - radiusX, cy - radiusY, diameterX, diameterY);
+        }
+    }
+
+    private CanvasOvalDrawing() {}
+}
--- javafx/src/main/java/engineer/mathsoftware/canvasplay/shape/drawing/OvalDrawing.java
+++ javafx/src/main/java/engineer/mathsoftware/canvasplay/shape/drawing/OvalDrawing.java
@@ -4,4 +4,31 @@
 
 package engineer.mathsoftware.canvasplay.shape.drawing;
 
-public interface OvalDrawing extends CommonDrawings.CommonDrawing {}
+import engineer.mathsoftware.canvasplay.shape.Oval;
+import javafx.scene.canvas.GraphicsContext;
+
+import static engineer.mathsoftware.canvasplay.shape.Oval.*;
+import static engineer.mathsoftware.canvasplay.shape.drawing.CanvasOvalDrawing.*;
+
+public interface OvalDrawing extends CommonDrawings.CommonDrawing {
+    static OvalDrawing of(GraphicsContext ctx, Oval oval) {
+        return switch (oval) {
+            case Circle circle -> new CircleDrawing(
+                ctx,
+                circle.radius(),
+                circle.cx(),
+                circle.cy(),
+                circle.diameter()
+            );
+            case Ellipse ellipse -> new EllipseDrawing(
+                ctx,
+                ellipse.radiusX(),
+                ellipse.radiusY(),
+                ellipse.cx(),
+                ellipse.cy(),
+                ellipse.diameterX(),
+                ellipse.diameterY()
+            );
+        };
+    }
+}
