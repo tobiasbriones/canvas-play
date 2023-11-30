8fde51948bdf8b667754a92c6a5442a359f41df4
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-11-30 16:33:14 -0600

    Add utility class mod Rulers

---
+++ javafx/src/main/java/engineer/mathsoftware/canvasplay/composition/model/Rulers.java
@@ -0,0 +1,25 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/canvas-playground
+
+package engineer.mathsoftware.canvasplay.composition.model;
+
+import java.util.Optional;
+
+public final class Rulers {
+    public record Ruler(double width, double height) {}
+
+    public record RulerSegment(
+        RulerSegmentOrientation orientation,
+        double radius,
+        double cx,
+        double cy,
+        Optional<String> text
+    ) {}
+
+    public enum RulerSegmentOrientation {
+        HRuler, VRuler
+    }
+
+    private Rulers() {}
+}
