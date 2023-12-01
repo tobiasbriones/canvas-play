b888edf8fad4264bf5856c0c85324742a5460aad
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-12-01 16:27:57 -0600

    Fix copyright license header repository name in module-info.java

--- canvas-play-javafx/canvas-play-java/src/main/java/module-info.java
+++ canvas-play-javafx/canvas-play-java/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2023 Tobias Briones. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
-// This file is part of https://github.com/tobiasbriones/blog
+// This file is part of https://github.com/tobiasbriones/canvas-play
 
 module engineer.mathsoftware.canvasplay {
     requires javafx.controls;
