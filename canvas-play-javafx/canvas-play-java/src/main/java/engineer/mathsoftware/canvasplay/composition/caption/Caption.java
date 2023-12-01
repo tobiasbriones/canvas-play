eaf409d96e025140a23af5f5168eef3398432137
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-12-01 17:49:02 -0600

    Use REM units in interface Caption

--- canvas-play-javafx/canvas-play-java/src/main/java/engineer/mathsoftware/canvasplay/composition/caption/Caption.java
+++ canvas-play-javafx/canvas-play-java/src/main/java/engineer/mathsoftware/canvasplay/composition/caption/Caption.java
@@ -4,15 +4,16 @@
 
 package engineer.mathsoftware.canvasplay.composition.caption;
 
+import engineer.mathsoftware.canvasplay.Metrics.Rem;
+
 public sealed interface Caption {
-    double TITLE_SIZE = 1.5;
+    Rem TITLE_SIZE = Rem.of(1.5);
 
     // Use this smaller size for embedded captions (i.e., captions are part
     // of the whole animation). If the font size is bigger (1.5), then the
     // caption is separate from the animation.
     // double TITLE_SIZE = 1.25;
-
-    double ABSTRACT_SIZE = TITLE_SIZE * 1.5;
+    Rem ABSTRACT_SIZE = Rem.of(TITLE_SIZE.value() * 1.5);
 
     record Title(String value) implements Caption {}
 
