7479f30906ea73da6276800d2c2f0edb6dd97d85
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-12-02 21:15:00 -0600

    Add test cases for drawing rectangle, round rectangle

---
+++ canvas-play-javafx/canvas-play-java/src/test/java/engineer/mathsoftware/canvasplay/drawing/shape/QuadrilateralDrawingTest.java
@@ -0,0 +1,83 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/canvas-play
+
+package engineer.mathsoftware.canvasplay.drawing.shape;
+
+import engineer.mathsoftware.canvasplay.CanvasTest;
+import engineer.mathsoftware.canvasplay.FxProdCanvas;
+import engineer.mathsoftware.canvasplay.drawing.shape.QuadrilateralDrawings.QuadrilateralDrawing;
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.Test;
+
+import static engineer.mathsoftware.canvasplay.shape.Quadrilateral.Rectangle;
+import static engineer.mathsoftware.canvasplay.shape.Quadrilateral.RoundRectangle;
+
+class QuadrilateralDrawingTest extends CanvasTest {
+    @Test
+    void drawRectangle() {
+        var color = Color.DARKSEAGREEN;
+
+        actualCanvas(canvas -> {
+            var prodCanvas = new FxProdCanvas(canvas, 1.0);
+
+            prodCanvas
+                .drawingCtx(QuadrilateralDrawing::of)
+                .apply(
+                    new Rectangle(
+                        200.0, 100.0, CANVAS_WIDTH / 2.0, CANVAS_HEIGHT / 2.0
+                    ))
+                .fill(color);
+        });
+
+        expected(ctx -> {
+            ctx.setFill(color);
+            ctx.fillRect(
+                CANVAS_WIDTH / 2.0 - 100,
+                CANVAS_HEIGHT / 2.0 - 50.0,
+                200.0,
+                100.0
+            );
+        });
+
+        match("centered greenish rectangle (200, 100)");
+    }
+
+    @Test
+    void drawRoundRectangle() {
+        var color = Color.DARKSEAGREEN;
+
+        actualCanvas(canvas -> {
+            var prodCanvas = new FxProdCanvas(canvas, 1.0);
+
+            prodCanvas
+                .drawingCtx(QuadrilateralDrawing::of)
+                .apply(
+                    new RoundRectangle(
+                        new Rectangle(
+                            200.0,
+                            100.0,
+                            CANVAS_WIDTH / 2.0,
+                            CANVAS_HEIGHT / 2.0
+                        ),
+                        24.0,
+                        24.0
+                    ))
+                .fill(color);
+        });
+
+        expected(ctx -> {
+            ctx.setFill(color);
+            ctx.fillRoundRect(
+                CANVAS_WIDTH / 2.0 - 100,
+                CANVAS_HEIGHT / 2.0 - 50.0,
+                200.0,
+                100.0,
+                24.0,
+                24.0
+            );
+        });
+
+        match("centered greenish round rectangle (200, 100), arc=24");
+    }
+}
