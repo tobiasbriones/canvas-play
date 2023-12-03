2ab67787528b0f1614edea70710f37ff27ac925f
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-12-03 15:05:32 -0600

    Add design issue note in interface Quadrilateral type RoundRectangle
    Round shapes must be compositions of original shapes to remove a design flaw with cycles. This takes efforts to design, test, and commit, so it's for the record, and to fix when needed in the future.

--- canvas-play-javafx/canvas-play-java/src/main/java/engineer/mathsoftware/canvasplay/shape/Quadrilateral.java
+++ canvas-play-javafx/canvas-play-java/src/main/java/engineer/mathsoftware/canvasplay/shape/Quadrilateral.java
@@ -17,6 +17,18 @@ public sealed interface Quadrilateral extends Shape {
         }
     }
 
+    // TODO A round shape has to be defined as an actual composition of the
+    //  original shape. It's wrong to define a RoundRectangle (etc.) as a
+    //  Quadrilateral, since it doesn't have exactly four sides, but more
+    //  like arcs as well. A round shape has to be defined via composition, i
+    //  .e., it has *four sides* and the four arcs, it can be built from a
+    //  Rectangle, arcs are substracted, so the round shape is created. With
+    //  this composition, the drawing is efficient as it just has to delegate
+    //  the drawing to the four Line denoting the straight rectangle as usual,
+    //  and then just draw the arcs. This also removes cycles that come from
+    //  this erroneous recursive design (i.e., a RoundRect is not actually a
+    //  Quadrilateral because it's not exactly four sides, but this requires
+    //  a Rectangle (an actual Quadrilateral) so it's a loop of deps).
     record RoundRectangle(
         Rectangle rectangle,
         double arcX,
