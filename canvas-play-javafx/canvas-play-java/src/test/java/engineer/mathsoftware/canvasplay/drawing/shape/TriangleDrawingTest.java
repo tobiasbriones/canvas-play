bd76b3f23400a951a00d3cff6ecc69ce2123f5e1
Tobias Briones <tobiasbriones.dev@gmail.com>
2023-12-03 22:20:22 -0600

    Add test cases for drawing equilateral/rounded triangles

---
+++ canvas-play-javafx/canvas-play-java/src/test/java/engineer/mathsoftware/canvasplay/drawing/shape/TriangleDrawingTest.java
@@ -0,0 +1,82 @@
+// Copyright (c) 2023 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/canvas-play
+
+package engineer.mathsoftware.canvasplay.drawing.shape;
+
+import engineer.mathsoftware.canvasplay.CanvasTest;
+import engineer.mathsoftware.canvasplay.FxProdCanvas;
+import engineer.mathsoftware.canvasplay.drawing.shape.TriangleDrawings.TriangleDrawing;
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.Test;
+
+import static engineer.mathsoftware.canvasplay.shape.Triangle.*;
+import static engineer.mathsoftware.canvasplay.shape.Triangle.EquilateralTriangle;
+import static engineer.mathsoftware.canvasplay.shape.Triangle.SQRT_3;
+
+class TriangleDrawingTest extends CanvasTest {
+    @Test
+    void drawEquilateralTriangle() {
+        var color = Color.GRAY;
+
+        actualCanvas(canvas -> {
+            var prodCanvas = new FxProdCanvas(canvas, 1.0);
+
+            prodCanvas
+                .drawingCtx(TriangleDrawing::of)
+                .apply(
+                    new EquilateralTriangle(
+                        200.0,
+                        CANVAS_WIDTH / 2.0,
+                        CANVAS_HEIGHT / 2.0
+                    ))
+                .fill(color);
+        });
+
+        expected(ctx -> {
+            var height = 200.0 * SQRT_3 / 2.0;
+
+            ctx.setFill(color);
+            ctx.fillPolygon(
+                new double[] {
+                    CANVAS_WIDTH / 2.0 - 100.0,
+                    CANVAS_WIDTH / 2.0 + 100.0,
+                    CANVAS_WIDTH / 2.0,
+                },
+                new double[] {
+                    CANVAS_HEIGHT / 2.0 + height / 2.0,
+                    CANVAS_HEIGHT / 2.0 + height / 2.0,
+                    CANVAS_HEIGHT / 2.0 - height / 2.0,
+                },
+                3
+            );
+        });
+
+        match("equilateral triangle centered color gray");
+    }
+
+    @Test
+    void drawEquilateralRoundTriangle() {
+        var color = Color.GRAY;
+
+        actualCanvas(canvas -> {
+            var prodCanvas = new FxProdCanvas(canvas, 1.0);
+
+            prodCanvas
+                .drawingCtx(TriangleDrawing::of)
+                .apply(
+                    new RoundTriangle(
+                        new EquilateralTriangle(
+                            200.0,
+                            CANVAS_WIDTH / 2.0,
+                            CANVAS_HEIGHT / 2.0
+                        ),
+                        24.0
+                    )
+                )
+                .fill(color);
+        });
+
+        matchImage("rounded-equilateral-triangle.png");
+    }
+}
