c6e7f5883a975cd1dbfb61552f8a8172dc5ef263
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-01-04 23:18:31 -0600

    Create package shape.rounded

---
+++ canvas-play-javafx/canvas-play-java/src/main/java/engineer/mathsoftware/canvasplay/shape/rounded/package-info.java
@@ -0,0 +1,5 @@
+// Copyright (c) 2024 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/canvas-play
+
+package engineer.mathsoftware.canvasplay.shape.rounded;
