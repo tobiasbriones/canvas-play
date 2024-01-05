93d54eb89c242fbc412e46ec92d4644a8f0481d4
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-01-04 23:45:35 -0600

    Create class NS RoundedDrawings

---
+++ canvas-play-javafx/canvas-play-java/src/main/java/engineer/mathsoftware/canvasplay/drawing/shape/RoundedDrawings.java
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/canvas-play
+
+package engineer.mathsoftware.canvasplay.drawing.shape;
+
+public final class RoundedDrawings {
+
+    private RoundedDrawings() {}
+}
