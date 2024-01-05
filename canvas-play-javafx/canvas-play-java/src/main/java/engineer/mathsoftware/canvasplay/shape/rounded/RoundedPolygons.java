cad15f891ad9750f8796db3a3c3f151e13be6984
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-01-05 16:35:28 -0600

    Add sealed interface RoundedQuadrilateral

--- canvas-play-javafx/canvas-play-java/src/main/java/engineer/mathsoftware/canvasplay/shape/rounded/RoundedPolygons.java
+++ canvas-play-javafx/canvas-play-java/src/main/java/engineer/mathsoftware/canvasplay/shape/rounded/RoundedPolygons.java
@@ -4,7 +4,26 @@
 
 package engineer.mathsoftware.canvasplay.shape.rounded;
 
+import engineer.mathsoftware.canvasplay.shape.Shape;
+
+import static engineer.mathsoftware.canvasplay.shape.Quadrilateral.*;
+
 public final class RoundedPolygons {
+    public sealed interface RoundedQuadrilateral extends Shape {
+        record RoundedRectangle(
+            Rectangle innerRectangle,
+            double arcX,
+            double arcY
+        ) implements RoundedQuadrilateral {
+            @Override
+            public double area() {
+                var borders =
+                    2.0 * (arcX * innerRectangle.height()) + 2.0 * (arcY * innerRectangle.width());
+                var ellipse = Math.PI * arcX * arcY;
+                return innerRectangle.area() + borders + ellipse;
+            }
+        }
+    }
 
     private RoundedPolygons() {}
 }
